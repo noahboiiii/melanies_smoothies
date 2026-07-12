ae737e4102773204cee555abd5081f6ac26ad803
Noah Esguerra <127748504+noahboiiii@users.noreply.github.com>
2026-07-12 21:32:48 +0100

    testing new url

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
